e6723428fa138ea6c8a0f6037a3d1cb60759e042
Subham Mondal <subhammondal2502@gmail.com>
2026-09-17 19:30:42 +0530

    Add invalid marks validation .

--- Student_Grade_Checker.py
+++ Student_Grade_Checker.py
@@ -3,3 +3,5 @@ students_name=input("enter students name : ")
 marks=int(input("enter total marks upto 100 : "))
 print(students_name)
 print("total marks : ", marks)
+if marks<0 or marks>100:
+    print("Invalide marks")
