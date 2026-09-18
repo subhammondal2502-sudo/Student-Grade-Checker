4792e510b19daee6c64a22fe23b925e5a5957c5b
Subham Mondal <subhammondal2502@gmail.com>
2026-09-18 19:37:54 +0530

    Update Student_Grade_Checker.py

--- Student_Grade_Checker.py
+++ Student_Grade_Checker.py
@@ -5,13 +5,10 @@ print(students_name)
 print("total marks : ", marks)
 if marks<0 or marks>100:
     print("Invalide marks")
-
 elif marks>=90 :
     print("grade : A")
-
 elif marks>=75 and marks<=89 :
     print("grade : B")
-
 elif marks>=50 and marks<=74 :
     print("grade : c")
 else:
